93f83c7548389c670c171acddf7a68589f789c06
Ian <38500123+IBanda@users.noreply.github.com>
2021-02-21 15:21:11 +0800

    use requestAnimationFrame

---
+++ src/utils/animateValue.ts
@@ -0,0 +1,17 @@
+export default function animateValue(
+  obj: HTMLDivElement,
+  start: number,
+  end: number,
+  duration: number
+) {
+  let startTimestamp = 0;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = String(Math.floor(progress * (end - start) + start));
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
